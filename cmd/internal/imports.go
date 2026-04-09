31ad5b1eef075cf010c9d317dade5d740bd32f0f
Yuan Teoh <45984206+Yuan325@users.noreply.github.com>
2026-04-08 21:35:53 -0700

    Update imports.go

--- cmd/internal/imports.go
+++ cmd/internal/imports.go
@@ -176,7 +176,7 @@ import (
 	_ "github.com/googleapis/mcp-toolbox/internal/tools/mysql/mysqllistactivequeries"
 	_ "github.com/googleapis/mcp-toolbox/internal/tools/mysql/mysqllisttablefragmentation"
 	_ "github.com/googleapis/mcp-toolbox/internal/tools/mysql/mysqllisttables"
-  _ "github.com/googleapis/mcp-toolbox/internal/tools/mysql/mysqllisttablestats"
+	_ "github.com/googleapis/mcp-toolbox/internal/tools/mysql/mysqllisttablestats"
 	_ "github.com/googleapis/mcp-toolbox/internal/tools/mysql/mysqllisttablesmissinguniqueindexes"
 	_ "github.com/googleapis/mcp-toolbox/internal/tools/mysql/mysqlsql"
 	_ "github.com/googleapis/mcp-toolbox/internal/tools/neo4j/neo4jcypher"
